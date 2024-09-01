5d3c76236fa7163f5971239aa78f5a7b4667b5b6
GoranK89 <goran.kolar7@gmail.com>
2024-09-01 20:37:59 +0200

    login and logout functions moved to context

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,44 +1,15 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { UserContext } from "../context/UserContext";
 import styles from "./Login.module.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [serverMessage, setServerMessage] = useState("");
-
-  const navigate = useNavigate();
+  const { login, serverMessage } = useContext(UserContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const loginUser = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
-        });
-
-        const data = await response.json();
-
-        if (!response.ok) {
-          throw new Error(`${data.error}`);
-        }
-
-        setServerMessage(`Logging in with: ${data.email}`);
-
-        //TODO: redirect to content page after 1.5 seconds
-        setTimeout(() => navigate("/"), 1500);
-      } catch (error) {
-        setServerMessage(`Error: ${error.message}`);
-      }
-    };
-
-    await loginUser();
-
+    await login(email, password);
     setEmail("");
     setPassword("");
   };
--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -1,18 +1,74 @@
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [accessToken, setAccessToken] = useState(null);
+  const [serverMessage, setServerMessage] = useState("");
+  const navigate = useNavigate();
 
-  const login = () => {};
+  const login = async (email, password) => {
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
-  const logout = () => {};
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(`${data.error}`);
+      }
+
+      setServerMessage(`Logging in with: ${data.email}`);
+
+      //TODO: redirect to content page after 1.5 seconds
+      setTimeout(() => navigate("/"), 1500);
+    } catch (error) {
+      setServerMessage(`Error: ${error.message}`);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (!response.ok) {
+        throw new Error(`${data.error}`);
+      }
+
+      setServerMessage(`${data.message}`);
+
+      setTimeout(() => navigate("/login"), 1000);
+    } catch (error) {
+      setServerMessage(`Error: ${error.message}`);
+    }
+  };
 
   return (
     <UserContext.Provider
-      value={{ user, setUser, accessToken, setAccessToken }}
+      value={{
+        user,
+        setUser,
+        accessToken,
+        setAccessToken,
+        login,
+        logout,
+        serverMessage,
+      }}
     >
       {children}
     </UserContext.Provider>
