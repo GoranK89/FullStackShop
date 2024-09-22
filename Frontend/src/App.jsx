96b900bc91ec102782eb89c94b29b9ebdfc27ebc
GoranK89 <goran.kolar7@gmail.com>
2024-09-22 16:10:00 +0200

    added seller dashboard, dashboard context

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
+import { DashboardProvider } from "./context/DashboardContext";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Register from "./components/Register";
@@ -7,34 +8,43 @@ import Login from "./components/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Store from "./components/Store";
 import SellerDashboard from "./components/SellerDashboard";
+import StoreSetup from "./components/DashboardComponents/StoreSetup";
+import Products from "./components/DashboardComponents/Products";
+import Orders from "./components/DashboardComponents/Orders";
 
 function App() {
   return (
     <UserProvider>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/store"
-            element={
-              <ProtectedRoute requiredRole={"buyer"}>
-                <Store />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute requiredRole={"seller"}>
-                <SellerDashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
+      <DashboardProvider>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/store"
+              element={
+                <ProtectedRoute requiredRole={"buyer"}>
+                  <Store />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute requiredRole={"seller"}>
+                  <SellerDashboard />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="store-setup" element={<StoreSetup />} />
+              <Route path="products" element={<Products />} />
+              <Route path="orders" element={<Orders />} />
+            </Route>
+          </Routes>
+        </div>
+      </DashboardProvider>
     </UserProvider>
   );
 }
@@ -42,5 +52,5 @@ function App() {
 export default App;
 
 /*
-Sellers who can: Register as Seller, Login, Setup their store, Add products, Edit products, Delete products, View Received Orders, Update Order Status (Pending, Shipped, Delivered), Buyers who can register, Login, Browse products by all sellers, Add products to cart, Checkout, View order history, View order status, Cancel order, View seller profile, View seller products
+Buyers who can register, Login, Browse products by all sellers, Add products to cart, Checkout, View order history, View order status, Cancel order, View seller profile, View seller products
 */
